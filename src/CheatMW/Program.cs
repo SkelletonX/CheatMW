aa05c873d4f684194aa3f4563c35eec28a5332cd
SkelletonX <kingofmustache@gmail.com>
2023-03-18 19:20:12 -0300

    Commit first

---
+++ src/CheatMW/Program.cs
@@ -0,0 +1,43 @@
+﻿using CheatMW.Speed;
+using Memory;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+class Program
+{
+    public static string procName = "speed";
+    static void Main()
+    {
+        try
+        {
+            Mem m = new Mem();
+
+
+
+            bool openProc = false;
+
+            openProc = m.OpenProcess(procName);
+
+            if (openProc)
+            {
+
+                int cash = m.ReadInt(MWAddresses.GameStats.GAME_CASH);
+                Console.WriteLine(cash);
+
+                m.WriteMemory(MWAddresses.GameStats.GAME_CASH, "int", "454");
+                m.WriteMemory(MWAddresses.GameStats.GAME_NAME, "string", "FUCK@");
+
+
+            }
+        }catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        Console.ReadKey();
+    }
+}
---
+++ src/CheatMW/Speed/MWAddresses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheatMW.Speed
+{
+    public static class MWAddresses
+    {
+        public struct GameStats
+        {
+
+            public const String GAME_CASH = "base+005AB214,2C,34,0,6C,68,D4"; //INT32
+
+            public const String GAME_NAME = "base+0053DEB4,64,3D4,D8,54,30,4,7F8"; //STRING
+        }
+    }
+}
